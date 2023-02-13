b5d607fb7ef6faa6e9bd0ef1760e1b238b5bc8aa
kevinwu_21 <1052961740@qq.com>
2023-02-13 23:38:09 +0800

    初步搭建KeeDoc

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -264,8 +264,8 @@ export default ({
     },
     algolia: {
       appId: 'USWUR2KHVL',
-      apiKey: 'c8d22bee216bd0cc8f45b8e483e8cdc0',
-      indexName: 'KeeDoc',
+      apiKey: '549f677e28705a1c21c017dd78f8bd29',
+      indexName: 'kee',
       placeholder: '请输入关键词',
       buttonText: '搜索'
     }
